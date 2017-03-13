e318ad6c1df546cdc8d3882dcd961b834444d3c8
Danilo Cardoso <Danilo Cardoso@DESKTOP-TTN8P7G>
2017-03-13 20:10:44 -0300

    CargoRepositorioTesteIncluir criado com sucesso

---
+++ src/Main/CargoRepositorioTesteIncluir.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+import Entidades.Cargo;
+import Repositorios.CargoRepositorio;
+
+/**
+ *
+ * @author Danilo Cardoso
+ */
+public class CargoRepositorioTesteIncluir {
+    public static void main(String[] args) {
+        Cargo cargo = new Cargo();
+        cargo.setDescricao("Vendedor");
+        CargoRepositorio cargoRepositorio = new CargoRepositorio();
+        cargoRepositorio.inserir(cargo);
+        System.out.println("Cargo inserido com sucesso!");
+    }
+}
